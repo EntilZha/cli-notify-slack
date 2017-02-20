3f6b2a675bc8d6c461b1e7ba4ba485b79cd9e688
Pedro Rodriguez <ski.rodriguez@gmail.com>
2017-02-20 12:36:07 -0700

    Improve output formatting

--- slacknotify/cli.py
+++ slacknotify/cli.py
@@ -3,14 +3,22 @@ import subprocess
 import os
 import socket
 import getpass
+import time
+import datetime
 from slacker import Slacker
 
 
 SLACK_API_TOKEN = os.environ.get('SLACK_API_TOKEN')
 SLACK_CHANNEL = os.getenv('SLACK_CHANNEL', '#experiments-status')
 
+COLORS = {
+    'green': '#36a64f',
+    'light-blue': '#5bc0de',
+    'red': '#c9302c'
+}
 
-def notify(message, slack_api_token=None, slack_channel=None):
+
+def notify(message, attachments, slack_api_token=None, slack_channel=None):
     if slack_api_token is None:
         slack_api_token = SLACK_API_TOKEN
 
@@ -18,22 +26,48 @@ def notify(message, slack_api_token=None, slack_channel=None):
         slack_channel = SLACK_CHANNEL
 
     slack = Slacker(slack_api_token)
-    slack.chat.post_message(slack_channel, message, username='Experiment Bot')
+    slack.chat.post_message(slack_channel, message, attachments=attachments, username='Experiment Bot')
+
+
+def create_attachment(title, message, color):
+    return {
+        'fallback': message,
+        'color': COLORS[color],
+        'text': message,
+        'title': title,
+        'mrkdwn_in': ['text']
+    }
 
 
 def shell(shell_command):
-    process = subprocess.run(shell_command, shell=True, stdout=subprocess.PIPE)
-    return process.stdout.decode('utf8')
+    process = subprocess.run(shell_command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return process.stdout.decode('utf8'), process.stderr.decode('utf8'), process.returncode
 
 
 def cli():
     hostname = socket.gethostname()
     user = getpass.getuser()
     command = ' '.join(sys.argv[1:])
-    result = shell(command)
-    notify('{user}@{hostname} $ {command}\n{result}'.format(
+    start_time = time.time()
+    stdout, stderr, returncode = shell(command)
+    total_time = time.time() - start_time
+    delta = str(datetime.timedelta(seconds=total_time))
+    attachments = []
+    input_command = 'User: `{user}`\tHostname: `{hostname}`\t Running TIme: {time}\nDirectory: `{pwd}`\n Command: `{command}`'.format(
         user=user,
         hostname=hostname,
+        pwd=os.getcwd(),
         command=command,
-        result=result
-    ))
+        time=delta
+    )
+    attachments.append(create_attachment('Command', input_command, 'light-blue'))
+    if stdout != '':
+        attachments.append(create_attachment('Standard Out', '```\n' + stdout + '```', 'green'))
+    else:
+        attachments.append(create_attachment('Standard Out', 'No Output', 'green'))
+    if returncode != 0:
+        if stderr != '':
+            attachments.append(create_attachment('Standard Error', '```\n' + stderr + '```', 'red'))
+        else:
+            attachments.append(create_attachment('Standard Error', 'No Output', 'red'))
+    notify('Command Completed', attachments)
